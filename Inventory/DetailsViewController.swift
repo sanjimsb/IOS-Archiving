230324003d2507753ae396db5c84ae2242a007a0
Bipin MSB <bpmsb45@gmail.com>
2022-03-26 14:17:06 -0400

    Initial project setup

---
+++ Inventory/DetailsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailsViewController.swift
+//  Inventory
+//
+//  Created by Cambrian on 2022-03-23.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var SKUField: UITextField!
+    @IBOutlet weak var descField: UITextField!
+    @IBOutlet weak var dateField: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Inventory/ItemList.swift
@@ -0,0 +1,25 @@
+//
+//  ItemList.swift
+//  Inventory
+//
+//  Created by Cambrian on 2022-03-23.
+//
+
+import Foundation
+
+class ItemList{
+    var items = [Item]()
+    
+    func addItem(item: Item){
+        // complete code
+    }
+    
+    func deleteItem(row: Int){
+        // complete code
+    }
+    
+    func moveItem(from: Int, to: Int){
+        // complete code
+    }
+    
+}
